6f5c3e7773d207db1ee06e56453d3880a5ca06a9
TomGieben <t.gieben@outlook.com>
2024-11-24 15:32:14 +0100

    fix obstacles

--- src/js/obstacles/Bee.js
+++ src/js/obstacles/Bee.js
@@ -1,7 +1,7 @@
 export class Bee {
     apply(element) {
-        element.style.width = '60px';
-        element.style.height = '60px';
+        element.style.width = '50px';
+        element.style.height = '50px';
         element.style.backgroundImage = 'url("/storage/images/bee.webp")';
         element.style.marginBottom = '30px';
         element.style.backgroundSize = 'contain';
--- src/js/obstacles/Goose.js
+++ src/js/obstacles/Goose.js
@@ -1,7 +1,8 @@
 export class Goose {
     apply(element) {
-        element.style.width = '80px';
+        element.style.width = '70px';
         element.style.height = '60px';
         element.style.backgroundImage = 'url("/storage/images/goose.webp")';
+        element.style.backgroundSize = 'contain';
     }
 }
